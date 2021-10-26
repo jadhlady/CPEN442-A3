4dcdc46a2d8cfd7e21f3f50f6b77239489880051
Jadon Hladyshevsky <jadhlady@gmail.com>
2021-10-25 20:29:23 -0700

    changed to right location

--- protocol.py
+++ protocol.py
@@ -1,29 +1,25 @@
 import random
 
+from app import Assignment3VPN
+
 class Protocol:
     # Initializer (Called from app.py)
     # TODO: MODIFY ARGUMENTS AND LOGIC AS YOU SEEM FIT
     def __init__(self):
+        pass
+        
+    # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
+    # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
+    def GetProtocolInitiationMessage(self):
         self.p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
         self.g = 2
         self.private_val = random.getrandbits(2048) 
         self.public_val = pow(self.g,self.private_val,self.p)
-        pass
-
-    def DH_check(self):
-        ##shared_key = pow(self.public_val_recieved (this value is recieved),self.private_val)
-        pass
-
-        
-
-    # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
-    # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
-    def GetProtocolInitiationMessage(self):
-        return ""
+        return self.public_val
 
 
     # Checking if a received message is part of your protocol (called from app.py)
-    # TODO: IMPLMENET THE LOGIC
+    # TODO: IMPLMENET THE LOGICs
     def IsMessagePartOfProtocol(self, message):
         return False
 
